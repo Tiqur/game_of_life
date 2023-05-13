ebc40ab81f22b4e860c1889fd8f32c8106c0db2b
Tiqur <trevorbrage02@gmail.com>
2023-05-12 20:45:44 -0700

    Create basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use minifb::{Key, Window, WindowOptions};
+
+const DIM: usize = 640;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer: Vec<u32> = vec![0; DIM*DIM];
+    let mut window = Window::new(
+        "Conway's Game Of Life",
+        DIM,
+        DIM,
+        WindowOptions::default()
+    ).unwrap();
+
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0;
+        }
+
+        window.update_with_buffer(&buffer, DIM, DIM)
+            .unwrap();
+    }
 }
