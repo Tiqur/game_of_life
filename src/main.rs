716ad9cdc65d4cd4e8437e63339f75e133b881b5
Tiqur <trevorbrage02@gmail.com>
2023-05-16 00:17:04 -0700

    Fix neighbor count function not checking diagonals

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use rand::Rng;
 use minifb::{Key, Window, WindowOptions};
 
 const DIM: usize = 512;
-const CELL_SIZE: usize = 32;
+const CELL_SIZE: usize = 8;
 const CELL_ROW_LEN: usize = DIM / CELL_SIZE;
 const FPS: u64 = 15;
 
 fn draw_world(buffer: &mut Vec<u32>, world: [[bool; CELL_ROW_LEN]; CELL_ROW_LEN], window: &mut Window) {
-    for y in 0..world.len() {
+    for y in 0..CELL_ROW_LEN {
         for x in 0..CELL_ROW_LEN {
             let cell_start_index = y * CELL_SIZE*DIM+x*CELL_SIZE;
-            let cell_color = if world[x][y] { 0 } else { 255 << 16 | 255 << 8 | 255 };
+            let cell_color = if world[y][x] { 0 } else { 255 << 16 | 255 << 8 | 255 };
             for x_cell in 0..CELL_SIZE {
                 for y_cell in 0..CELL_SIZE {
                     buffer[cell_start_index+x_cell+y_cell*DIM] = cell_color;
@@ -21,34 +22,57 @@ fn draw_world(buffer: &mut Vec<u32>, world: [[bool; CELL_ROW_LEN]; CELL_ROW_LEN]
         .unwrap();
 }
 
-fn get_live_neighbor_count(cell_pos: (usize, usize), world: &[[bool; CELL_ROW_LEN]; CELL_ROW_LEN]) -> u8 {
+fn get_live_neighbor_count(cell_pos: (usize, usize), world: [[bool; CELL_ROW_LEN]; CELL_ROW_LEN]) -> u8 {
     let mut live_neighbor_count = 0;
     let x = cell_pos.0;
     let y = cell_pos.1;
+    let x32 = x as i32;
+    let y32 = y as i32;
 
-    if x > 0 && world[x - 1][y] { live_neighbor_count += 1 };
-    if x < CELL_ROW_LEN - 1 && world[x + 1][y] { live_neighbor_count += 1 };
-    if y > 0 && world[x][y - 1] { live_neighbor_count += 1 };
-    if y < CELL_ROW_LEN - 1 && world[x][y + 1] { live_neighbor_count += 1 };
+    for i in -1..=1 {
+        for j in -1..=1 {
+            if i == 0 && j == 0 {
+                continue;
+            }
+
+            let new_x32 = x32.checked_add(j);
+            let new_y32 = y32.checked_add(i);
+
+            if let (Some(new_x), Some(new_y)) = (new_x32, new_y32) {
+                let new_x_usize = new_x as usize;
+                let new_y_usize = new_y as usize;
+
+                if let Some(cell) = world.get(new_y_usize).and_then(|row| row.get(new_x_usize)) {
+                    if *cell {
+                        live_neighbor_count += 1;
+                    }
+                }
+            }
+        }
+    }
 
     live_neighbor_count
-}
+}fn get_updated_world(world: [[bool; CELL_ROW_LEN]; CELL_ROW_LEN]) -> [[bool; CELL_ROW_LEN]; CELL_ROW_LEN] {
+    let mut next_world: [[bool; CELL_ROW_LEN]; CELL_ROW_LEN] = [[false; CELL_ROW_LEN]; CELL_ROW_LEN];
 
-fn update_world(world: &mut [[bool; CELL_ROW_LEN]; CELL_ROW_LEN]) {
-    for x in 0..world.len() {
-        for y in 0..CELL_ROW_LEN {
+    for y in 0..CELL_ROW_LEN {
+        for x in 0..CELL_ROW_LEN {
             let live_neighbor_count = get_live_neighbor_count((x, y), world);
-            world[x][y] = match (world[x][y], live_neighbor_count) {
-                (true, 2..=3) => true,      // Live cell with 2 or 3 live neighbors survives
-                (true, _) => false,         // Live cell with fewer than 2 or more than 3 live neighbors dies
-                (false, 3) => true,         // Dead cell with exactly 3 live neighbors becomes alive
-                _ => world[x][y],           // For all other cases, the cell state remains the same
+            next_world[y][x] = match (world[y][x], live_neighbor_count) {
+                (true, 2..=3) => true,    // Live cell with 2 or 3 live neighbors survives
+                (true, _) => false,       // Live cell with fewer than 2 or more than 3 live neighbors dies
+                (false, 3) => true,       // Dead cell with exactly 3 live neighbors becomes alive
+                _ => next_world[y][x],    // For all other cases, the cell state remains the same
             };
         }
     }
+
+    next_world
 }
 
 fn main() {
+    let mut rng = rand::thread_rng();
+
     // Define window information
     let mut buffer: Vec<u32> = vec![0; DIM*DIM];
     let mut window = Window::new(
@@ -61,18 +85,53 @@ fn main() {
     // Init world
     let mut world: [[bool; CELL_ROW_LEN]; CELL_ROW_LEN] = [[false; CELL_ROW_LEN]; CELL_ROW_LEN];
 
-    world[2][2] = true;
-    world[2][3] = true;
-    world[3][2] = true;
-    world[3][3] = true;
+
+    //world[10][10] = true;
+    //world[11][11] = true;
+    //world[10][12] = true;
+    //world[11][13] = true;
+    //world[10][14] = true;
+
+    // Set random
+    for y in 0..CELL_ROW_LEN {
+        for x in 0..CELL_ROW_LEN {
+            let random = rng.gen();
+            world[x][y] = random;
+        }   
+    }
+    
+    //world[10][10] = true;
+    //world[10][11] = true;
+    //world[11][10] = true;
+    //world[11][11] = true;
+
+    // Blinker
+    //world[10][10] = true;
+    //world[10][11] = true;
+    //world[10][12] = true;
+    
+    // Toad
+    //world[9][11] = true;
+    //world[10][10] = true;
+    //world[10][11] = true;
+    //world[11][10] = true;
+    //world[11][11] = true;
+    //world[12][10] = true;
+
+    // Glider
+    //world[10][10] = true;
+    //world[10][11] = true;
+    //world[10][12] = true;
+    //world[9][12] = true;
+    //world[8][11] = true;
 
     // Time to sleep
     let frame_duration = std::time::Duration::from_secs_f64(1.0 / FPS as f64);
 
     // Graphics loop
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        std::thread::sleep(frame_duration);
-        update_world(&mut world);
         draw_world(&mut buffer, world, &mut window);
+        world = get_updated_world(world);
+        std::thread::sleep(frame_duration);
     }
 }
